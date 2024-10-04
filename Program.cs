3fb3f16568d9eb61f80f8eef8bb2ceeb966f51c0
Zalan888 <witmannz@kkszki.hu>
2024-10-04 11:34:16 +0200

    input method added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penztarr
+{
+    internal class Program
+    {
+        public static int[] notes = new int[6] { 500, 1000, 2000, 5000, 10000, 20000 };
+        public static int[] money = new int[6];
+
+        public static void input()
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.WriteLine($"Kérem adja meg a {notes[i]}-asok számát!");
+                money[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void Main(string[] args)
+        {
+            input();
+        }
+    }
+}
